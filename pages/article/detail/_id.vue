c7581cb13c3fafc2229d48bc8d8873e562cfe378
香饽饽zizizi <863461783@qq.com>
2019-01-29 10:17:22 +0800

    fix

--- pages/article/detail/_id.vue
+++ pages/article/detail/_id.vue
@@ -99,7 +99,7 @@ export default {
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: this.wechatparams.appId, // 必填，公众号的唯一标识
         timestamp: this.wechatparams.timestamp, // 必填，生成签名的时间戳
-        nonceStr: this.wechatparams.nonceStr, // 必填，生成签名的随机串
+        nonceStr: this.wechatparams.noncestr, // 必填，生成签名的随机串
         signature: this.wechatparams.signature,// 必填，签名
         jsApiList: ['onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
       })
